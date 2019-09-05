7ad1d076f296bd13b814ce145d2b4b2909647c81
Akira <54892411+docxed@users.noreply.github.com>
2019-09-06 00:50:51 +0700

    Update Donut.py

--- Donut.py
+++ Donut.py
@@ -1,16 +1,13 @@
 """main"""
 def main():
     """main function"""
-    prize = int(input())
-    buy = int(input())
-    getfree = int(input())
-    want = int(input())
-    buyget = buy + getfree
-    pay1 = (want//buyget)*((prize*buy))+(want%buyget)*prize
-    pay2 = ((want//buyget)+1)*(prize*buy)
-    if pay1 >= pay2:
-        print(pay2, buyget*((want//buyget)+1))
+    price, buy, get, want = int(input()), int(input()), int(input()), int(input())
+    getfree = buy + get
+    sale = (want//getfree) * (price*buy) + (want%getfree) * price
+    sales = ((want//getfree) + 1) * (price*buy)
+    if sale >= sales:
+        print(sales, getfree * ((want//getfree) + 1))
     else:
-        print(pay1, want)
-
+        print(sale, want)
+ 
 main()
