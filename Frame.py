bb452e988c7cb6f107c2a8434f354dfbc897196d
Akira <54892411+docxed@users.noreply.github.com>
2019-09-05 12:01:24 +0700

    Create Frame.py

---
+++ Frame.py
@@ -0,0 +1,9 @@
+"""main"""
+def main():
+    """main funtion"""
+    txt = input()
+    print("*" + ("*"*len(txt)) + "*")
+    print("*" + (txt) + "*")
+    print("*" + ("*"*len(txt)) + "*")
+ 
+main()
