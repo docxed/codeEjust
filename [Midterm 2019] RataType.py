ede701d42fa796be3e470ca4315e1d4f414302f3
Akira <54892411+docxed@users.noreply.github.com>
2019-10-04 01:27:30 +0700

    Create [Midterm 2019] RataType.py

---
+++ [Midterm 2019] RataType.py
@@ -0,0 +1,17 @@
+"""main"""
+def main():
+    """main"""
+    world = [
+        "",
+        "In Deep Learning, a Convolutional Neural Network (CNN) is a class of Deep Neural \
+Networks, most commonly applied to analyzing visual imagery.",
+        "\"Two things are infinite: the universe and human stupidity; and I'm not sure about the \
+universe\". - Albert Einstein",
+        "Statistics is the discipline that concerns the collection, organization, displaying, \
+analysis, interpretation and presentation of data.",
+        "The backslash (\\) is a typographical mark used mainly in computing and is the mirror \
+image of the common slash (/). It is sometimes called \"escape character\"."
+    ]
+    print(world[int(input())])
+ 
+main()
