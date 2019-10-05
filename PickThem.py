78c2e265fb617000a08e5cea2f49de5035acbedb
Akira <54892411+docxed@users.noreply.github.com>
2019-10-06 01:24:06 +0700

    Update PickThem.py

--- PickThem.py
+++ PickThem.py
@@ -2,20 +2,16 @@
 def main():
     """main"""
     num = input().split()
-    new, tmp, last = [], "", []
+    new, tmp = [], ""
     for i in range(len(num)):
         for j in num[i]:
             if j.isdigit():
                 tmp += j
         new.append(tmp)
         tmp = ""
-    new = list(map(int, new))
-    for k in new:
-        if k % 2 == 0:
-            last.append(k)
-    if last == []:
+    new = list(filter(lambda num: int(num) % 2 == 0, new))
+    _ = [print(h) for h in new if new != []]
+    if new == []:
         print("Nope")
-        return
-    _ = [print(h) for h in last]
  
 main()
